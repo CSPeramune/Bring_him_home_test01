9d70e0a516921f4762f3bc49465a8f0a7868f927
Chamith Peramune <49214952+CSPeramune@users.noreply.github.com>
2019-06-08 17:51:30 +0530

    Correct the player movement

--- Assets/player_mov.cs
+++ Assets/player_mov.cs
@@ -6,6 +6,7 @@ public class player_mov : MonoBehaviour
 {
     public Rigidbody rb;
     public float forwardF = 200f;
+    public float moveSpeed;
 
     void Start() { 
     }
@@ -13,17 +14,17 @@ public class player_mov : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKey("d")){
-            rb.AddForce(0, 0, forwardF * Time.deltaTime);
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a")){
+            transform.Translate(new Vector3 (0, 0, -moveSpeed) * Time.deltaTime);
         }
-        if(Input.GetKey("s")){
-            rb.AddForce(forwardF * Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")){
+            transform.Translate(new Vector3 (0, 0, moveSpeed) * Time.deltaTime);
         }
-        if(Input.GetKey("a")){
-            rb.AddForce(0, 0, -forwardF * Time.deltaTime);
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w")){
+            transform.Translate(new Vector3 (-moveSpeed, 0, 0) * Time.deltaTime);
         }
-        if(Input.GetKey("w")){
-            rb.AddForce(-forwardF * Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")){
+            transform.Translate(new Vector3 (moveSpeed, 0, 0) * Time.deltaTime);
         }
     }
 }
