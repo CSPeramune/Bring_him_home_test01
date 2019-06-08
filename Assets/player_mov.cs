0e28d8cbdbe1cda4d3901b415e8f5d3faa7b8f9b
Chamith Peramune <49214952+CSPeramune@users.noreply.github.com>
2019-06-08 08:47:15 +0530

    Add force to player

---
+++ Assets/player_mov.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_mov : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardF = 200f;
+
+    void Start() { 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d")){
+            rb.AddForce(0, 0, forwardF * Time.deltaTime);
+        }
+        if(Input.GetKey("s")){
+            rb.AddForce(forwardF * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(0, 0, -forwardF * Time.deltaTime);
+        }
+        if(Input.GetKey("w")){
+            rb.AddForce(-forwardF * Time.deltaTime, 0, 0);
+        }
+    }
+}
