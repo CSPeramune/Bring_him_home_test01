48b8477368c1abfb583a0d65e327a708e235d2ab
Chamith Peramune <49214952+CSPeramune@users.noreply.github.com>
2019-06-09 08:06:59 +0530

    Add cameraFollow.cs
    Now camera is moving when player move.... :blush:

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public float xDifference;
+    public float zDifference;
+    public float movementThreshold = 5f;
+
+    void FixedUpdate()
+    {
+        /*if(target.transform.position.x > transform.position.x){
+            xDifference = target.transform.position.x - transform.position.x;
+        }
+        else{
+            xDifference = transform.position.x - target.transform.position.x;
+        }
+        if(target.transform.position.z > transform.position.z){
+            zDifference = target.transform.position.z - transform.position.z;
+        }
+        else{
+            zDifference = transform.position.z - target.transform.position.z;
+        }
+        
+        if(xDifference >= movementThreshold || zDifference >= movementThreshold){*/
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        //transform.LookAt(target);
+       //}
+    }
+}
